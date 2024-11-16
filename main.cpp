a370b5215363bda0b178ce536e14e6491fe23e32
ecruanes <ecruanes@bu.edu>
2024-11-16 16:01:02 -0500

    update

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #endif
 
 // initialize LCD
-LiquidCrystal_I2C lcd(0x20,16,2); // set the LCD address to 0x20 for a 16 chars and 2 line display
+//LiquidCrystal_I2C lcd(0x20,16,2); // set the LCD address to 0x20 for a 16 chars and 2 line display
 
 // initialize button pins
 const int moveRedButton;
@@ -239,10 +239,6 @@ void setup() {
   }
 
 }
-
-
-
-
 
 
 

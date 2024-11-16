fceec3ae503c248c4195d7dd2657148fc2b6e705
ecruanes <ecruanes@bu.edu>
2024-11-16 14:59:10 -0500

    updated with dice led pad

--- arduino.cpp
+++ arduino.cpp
@@ -1,16 +1,23 @@
 // libraries
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_NeoPixel.h>
 
 /// initialize LCD
 LiquidCrystal_I2C lcd(0x20,16,2); // set the LCD address to 0x20 for a 16 chars and 2 line display
+
+#define PIN        2
+#define NUMPIXELS 256// Popular NeoPixel ring size
 
 // button pins
 const int moveRedButton;
 const int moveYellowButton;
 const int moveGreenButton;
 const int moveBlueButton;
-const int rollButton;
+const int diceRoll = 10 ; 
+
+//button state 
+int button_state; 
 
 // LED matrix pins
 
@@ -24,6 +31,101 @@ const int gameBoardSize = 40; // can change the how many spaces on board
 int rollDie() {
   return random(1,7);
 }
+
+void homeBase(){ 
+  pixels.setPixelColor(0, pixels.Color(0, 150, 0)); // GREEN HOME 
+    pixels.show(); 
+    pixels.setPixelColor(15, pixels.Color(150, 0, 0)); // BLUE HOME 
+    pixels.show();   // Send the updatedpixel colors to the hardware.
+    pixels.setPixelColor(240, pixels.Color(0, 0, 150)); // RED HOME 
+    pixels.show();   // Send the updatedpixel colors to the hardware.
+    pixels.setPixelColor(255, pixels.Color(150, 150, 0)); //YELLOW  HOME 
+   pixels.show();   // Send the updatedpixel colors to the hardware. 
+   delay(DELAYVAL); // Pause before next pass through loop
+}
+
+void pixelPadClear(){ 
+  for(int i=103; i<106; i++){ 
+    pixels.setPixelColor(i, pixels.Color(0,0,0)); // GREEN HOME 
+    pixels.show(); 
+  }
+  for(int i=118; i<121; i++){ 
+    pixels.setPixelColor(i, pixels.Color(0,0,0)); // GREEN HOME 
+    pixels.show(); 
+  }
+  for(int i=135; i<137; i++){ 
+    pixels.setPixelColor(i, pixels.Color(0,0,0)); // GREEN HOME 
+    pixels.show(); 
+  }
+}
+
+void setDicePad(int randomNumber){ 
+  Serial.println("DICE BUTTON PRESSED"); 
+      //CLEAR DICE PAD 
+      pixelPadClear(); 
+      Serial.println(randomNumber); 
+      if(randomNumber == 1){ 
+        pixels.setPixelColor(119, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+      }
+      else if(randomNumber == 2){ 
+        pixels.setPixelColor(103, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(137, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+      }
+      else if(randomNumber == 3){ 
+        pixels.setPixelColor(119, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(103, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(137, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+      }
+      else if(randomNumber == 4){ 
+        pixels.setPixelColor(103, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(105, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(137, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(103, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(135, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+      }
+      else if(randomNumber == 5){ 
+        pixels.setPixelColor(103, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(105, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(137, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(103, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(135, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(119, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+      }
+      else if(randomNumber == 6){ 
+        pixels.setPixelColor(103, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(105, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(137, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(135, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(118, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        pixels.setPixelColor(120, pixels.Color(150, 150, 150)); // GREEN HOME 
+        pixels.show(); 
+        
+      }
+      delay(1000); 
+      dice_button_state = 0; 
+    }
 
 // this class represents the Players' board piece
 class Piece {
@@ -98,22 +200,20 @@ void setup() {
   }*/
   lcd.clear();
   lcd.print("Roll the die!");
-  
+  pixels.begin(); 
+  //SET HOME BASES 
+  homeBase(); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  button_state= digitalRead(diceRoll); 
   Serial.println("Press the button to roll the die");
   while (digitalRead(diceRoll) == LOW) {
     // does nothing until button is HIGH
   }
 
   delay(50);
-
-   int randomNumber = random(1, 7); // Generate random integer between 1 and 6
-  Serial.print("You rolled: ");
-  Serial.println(randomNumber);
 
   if (randomNumber == 6) {
     Serial.println("Color starts.");
@@ -125,8 +225,12 @@ void loop() {
   }
 
   // Wait for button release
-  while (digitalRead(diceRoll) == HIGH) {
-    // Does nothing until button is released (HIGH state)
+  while (button_state == HIGH) {
+    int randomNumber = rollDie(); 
+    lcd.clear();    
+    lcd.print("ROLLED A ");  // Print the string
+    lcd.print(randomNumber); // Print the value of randomNumber
+    setDicePad(randomNumber); 
   }
 
   delay(50); // Debounce delay to prevent false triggers

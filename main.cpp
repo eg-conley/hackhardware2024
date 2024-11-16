0f20d082038f5b065eb11387b8b2921e736da869
Ella Conley <egconley@bu.edu>
2024-11-16 15:53:44 -0500

    changes

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ const int moveYellowButton;
 const int moveGreenButton;
 const int moveBlueButton;
 const int diceButton = 10 ;
+int diceButtonState;
 const int buzzer = 8; //buzzer to arduino pin 8 for victory music 
 
 // initialize led matrix
@@ -27,8 +28,6 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 const int numPlayers = 4;
 const int gameBoardSize = 40; // can change the how many spaces on board
 const int offBoardPos = 50; // can change depending on board size
-Piece* Players[numPlayers];
-int currPlayerIndex = 0; // default starting player is Red
 
 // functions and Piece class
 // this function generates a random integer between 1 and 6
@@ -184,7 +183,7 @@ void setDicePad(int roll){
         
       }
       delay(1000); 
-      dice_button_state = 0; 
+      diceButtonState = 0; 
     }
 
 void setup() {
@@ -193,6 +192,152 @@ void setup() {
  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
  homeBase(); // set home base
 
+  Serial.begin(9600);
+  
+  // initialize random number generator
+  randomSeed(analogRead(0));
+
+  // create board with indices
+  int gameBoard[gameBoardSize];
+  for (int i = 0; i < gameBoardSize; i++)
+    gameBoard[i] = i;
+}
+
+  // create all 4 pieces and initialize corresponding board values
+  Piece* Red = new Piece("red", 1, 40);
+  Piece* Yellow = new Piece("yellow", 11, 10);
+  Piece* Green = new Piece("green", 21, 20);
+  Piece* Blue = new Piece("blue", 31, 30);
+  Piece* Players[numPlayers] = {Red, Yellow, Green, Blue};
+  int currPlayerInd = 0; // default starting player is Red
+  Piece* currPlayer = Players[currPlayerInd]; // default first player is red
+  
+
+  void loop() {
+    diceButtonState = digitalRead(diceButton);
+    while (diceButtonState == HIGH) {
+      int roll = rollDie(); 
+      setDicePad(roll); 
+      
+      Serial.print("Player ");
+      Serial.print(currPlayer->colorId);
+      Serial.print("rolled: ");
+      Serial.println(roll);
+
+      currPlayer->takeTurn(roll); // start the player's turn with their roll
+  
+      if (currPlayer->hasWon) {
+        Serial.print("Player ");
+        Serial.print(currPlayer->colorId);
+        Serial.println(" has won!");
+        while (true) {} // ends the game if someone wins
+      }
+
+      // otherwise move to the next player
+      currPlayerInd = (currPlayerInd + 1) % numPlayers;
+      delay (500); // debouncer 
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*// libraries
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// initialize LCD
+LiquidCrystal_I2C lcd(0x20,16,2); // set the LCD address to 0x20 for a 16 chars and 2 line display
+
+// button pins
+const int moveRedButton;
+const int moveYellowButton;
+const int moveGreenButton;
+const int moveBlueButton;
+const int rollButton;
+
+// LED matrix pins
+
+// other variables
+const int numPlayers = 4;
+const int gameBoardSize = 40; // can change the how many spaces on board
+const int offBoardPos = 50; // can change depending on board size
+Piece* Players[numPlayers];
+int currPlayerIndex = 0; // default starting player is Red
+
+// functions and Piece class
+// this function generates a random integer between 1 and 6
+int rollDie() {
+  return random(1,7);
+}
+
+// this class represents the Players' board piece
+class Piece {
+  public:
+    // ATTRIBUTES
+    String colorId;
+    int currPos; // this will be set to a value not on the board (ex: 50)
+    int startPos;
+    int endPos;
+    bool isHome;
+    bool hasWon;
+
+    // METHODS
+    // constructor
+    Piece(String color, int start, int end) : colorId(color), currPos(offBoardPos), startPos(start), endPos(end), isHome(true), hasWon(false) {}
+
+    // this function moves the piece forward one space on the board
+    void moveOne() {
+      // if statement prevents piece from moving forward if it is at its end position on the board
+      if ((currPos+1) % gameBoardSize != endPos) // modulus functionality to create circularity for board
+        currPos = (currPos + 1) % gameBoardSize;
+    }
+
+    // this function checks if another piece is in the same position on the board and sends that piece back to home
+    void checkOverlaps(Piece* otherPiece) {
+      if (currPos == otherPiece->currPos) {
+        otherPiece->isHome = true;
+        otherPiece->currPos = offBoardPos;
+      }
+    }
+
+    // this function starts the corresponding Player's turn
+    void takeTurn(int moveCount) {
+      // if the piece is home, need to roll a 6 to get on board
+      if (isHome) {
+        if (moveCount == 6) {
+          isHome = false;
+          currPos = startPos; // puts piece on the board
+        }
+      }
+      // if the piece is at end, need to roll a 1 to win
+      else if (currPos == endPos -1) {
+        if (moveCount == 1) {
+          hasWon = true;
+        }
+      }
+      // if piece is on board, roll
+      else {
+        for (int i = 0; i < moveCount; i++)
+          moveOne();
+      }
+    }
+};
+
+// setup code runs once
+void setup() {
   Serial.begin(9600);
   
   // initialize random number generator
@@ -212,16 +357,17 @@ void setup() {
 
 
 void loop() {
-    while (digitalRead(diceButton) == HIGH) {
-      int roll = rollDie(); 
-      setDicePad(roll); 
+  Piece* currPlayer = Players[currPlayerInd];
 
-      Serial.print("Player ");
-      Serial.print(currPlayer->colorId);
-      Serial.print("rolled: ");
-      Serial.println(roll);
+  // wait for player to press roll button to roll the die
+  if (digitalRead(rollButton == HIGH)) {
+    int roll = rollDie();
+    Serial.print("Player ");
+    Serial.print(currPlayer->colorId);
+    Serial.print("rolled: ");
+    Serial.println(roll);
 
-      currPlayer->takeTurn(roll); // start the player's turn with their roll
+    currPlayer->takeTurn(roll); // start the player's turn with their roll
   
     if (currPlayer->hasWon) {
       Serial.print("Player ");
@@ -234,5 +380,9 @@ void loop() {
     currPlayerInd = (currPlayerInd + 1) % numPlayers;
     delay (500); // debouncer 
   }
-}
+
+}*/
+
+
+
 
